c740d40226acf773b63a6a02dceabeb72d7a072a
FediiBohdan <gizelbard@ukr.net>
2022-12-02 11:27:20 +0200

    Initial commit

---
+++ src/main/java/com/example/ec2testapp/Ec2TestAppApplication.java
@@ -0,0 +1,21 @@
+package com.example.ec2testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Ec2TestAppApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to AWS";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2TestAppApplication.class, args);
+    }
+
+}
